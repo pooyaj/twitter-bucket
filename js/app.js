83f1c058159193ebe9cb9e601e7502c74abb503b
Pooya Jaferian <pooya.j@gmail.com>
2014-08-31 00:00:43 -0700

    updated the look and feel so it looks like a tweeter timeline

--- js/app.js
+++ js/app.js
@@ -9,15 +9,26 @@ var TweetsCollection = Backbone.Collection.extend({
     this.query = options.query;
   },
   url: function () {
-
     return "/tweetproxy/proxy.php?q="+ this.query;
   },
   parse: function(response) {
     return response.statuses;
+  }, 
+  update: function() {
+    this.fetch({
+      add: true;
+    });
   }
 });
 
-var tweetTemplate = _.template('<%= model.get("text") %>');
+
+var tweetTemplate = _.template('<time class="tweet_time"><span><%= model.get("created_at").slice(0,10) %></span> <span><%= model.get("created_at").slice(10,16) %></span></time>'+
+'<div class="tweet_user" style="background-image: url(\'<%= model.get("user").profile_background_image_url %>\')"></div>'+
+'<div class="tweet_body"><p><%= fixURL(model.get("text")) %></p></div>');
+
+
+
+
 
 var TweetView = Backbone.View.extend({
   tagName: 'li',
@@ -32,20 +43,39 @@ var TweetView = Backbone.View.extend({
     this.$el.html( this.template( this ) );
     return this.$el;
   },
-
+  fixURL: function(text){    
+    return text.replace(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi, function(text){
+      return text.link(text);
+    });
+  }
 });
 
 var TweetsList = Backbone.View.extend({
   el: $('.tweets'),
   initialize: function(options){
-    if (options.collection) this.collection = options.collection; else this.collection = new TweetsCollection();
+    this.collection = new TweetsCollection([], {query: options.query});
     this.listenTo( this.collection, 'add', this.renderTweet );
-    this.collection.fetch();
+    this.listenTo( this.collection, 'reset', this.reset );
   },
   renderTweet: function(model){
     model.view = new TweetView({ model: model });
-    this.$('#tweet-list').prepend( model.view.render() );
+    this.$('#tweet-list').append( model.view.render() );
   },
+  reset: function() {
+    this.$('#tweet-list').empty();
+  },
+  updateSearch: function(query) {
+    this.collection.query = query;
+    this.collection.reset();
+    this.collection.fetch();
+  },
+  events: {
+    'click #searchButton': 'doSearch'
+  },
+  doSearch: function() {
+    var query = $('#searchBox').val();
+    tweetRouter.navigate("#/search/"+query, {trigger: true});
+  }
 });
 
 // creating the event handler
@@ -57,27 +87,23 @@ var AppRouter = Backbone.Router.extend({
       '': 'index'
     },
     search: function(query) {
-      console.log("search:" + query);
       vent.trigger("search:query", query);
     },
     index: function()  {
-      console.log("index");
+      if (!window.tweets) createCollections();
     }
   });
 
 vent.on("search:query", function(query) {
-  console.log("Here you go :"+query);
+  if (window.tweets) window.tweets.updateSearch(query); else createCollections(query);
 });
 
-$(function(){
+function createCollections(query) {
+  var tweetCollection = new TweetsCollection([], {query: query});
+  window.tweets = new TweetsList({collection: tweetCollection});
+}
 
-/*  
-  $('#searchButton').click(function() {
-    var tweetCollection = new TweetsCollection([], {query: $('#searchBox').val()});
-    window.tweets = new TweetsList({collection: tweetCollection});
-    tweetCollection.fetch();
-  });
-*/
+$(function(){
   window.tweetRouter = new AppRouter();
   Backbone.history.start();
 });
