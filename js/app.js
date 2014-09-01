a302f6fce373e4d0412653191dbd34e0c38540cb
Pooya Jaferian <pooya.j@gmail.com>
2014-08-31 19:47:24 -0700

    fixed the issue with the tweet appearing in the reverse order. Added a feature to stage the tweets before revealing them. Updated the look and feel of the search box. Also used the twitter entities info to process urls rather than a manual method

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
+// Model for an individual tweet
 var TweetModel = Backbone.Model.extend({
   validate: function(attrs){
   }
 });
 
+
+// Model for list of tweets 
 var TweetsCollection = Backbone.Collection.extend({
   model: TweetModel,
+  comparator: function(model) {
+    return model.get('text');
+  },
   initialize: function(model, options) {
     this.query = options.query;
   },
   url: function () {
-    return "/tweetproxy/proxy.php?q="+ this.query+"&count=50";
+    return "/tweetproxy/proxy.php?q="+ this.query+"&count=5";
   },
   parse: function(response) {
     return response.statuses;
@@ -17,7 +23,7 @@ var TweetsCollection = Backbone.Collection.extend({
   update: function() {
     console.log("updating...");
     this.fetch({
-      add: true
+      remove: false
     });
   }
 });
@@ -40,40 +46,70 @@ var TweetView = Backbone.View.extend({
     this.$el.html( this.template( this ) );
     return this.$el;
   },
-  fixURL: function(text){    
-    return text.replace(/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi, function(text){
-      return text.link(text);
-    });
-  }
-});
+  fixURL: function(text){
+      console.log(this.model.get('entities').urls);
+      var urlUpdater = function (item) {
+        text = text.replace(text.slice(item.indices[0], item.indices[1]), "<a href='" + item.url + "'>" + item.display_url + "</a>");
+      };
+      this.model.get('entities').urls.forEach(urlUpdater);
+      if (this.model.get('entities').media) this.model.get('entities').media.forEach(urlUpdater);
+      return text;
+    }
+  });
 
 var TweetsList = Backbone.View.extend({
   el: $('.tweets'),
   initialize: function(options){
+    console.log("tweetlist:initialize");
     this.collection = new TweetsCollection([], {query: options.query});
     this.listenTo( this.collection, 'add', this.renderTweet );
-    this.listenTo( this.collection, 'reset', this.reset );
+    this.listenTo( this.collection, 'reset', this.reset);
   },
   renderTweet: function(model){
+    console.log("tweetlist:renderTweet");
     model.view = new TweetView({ model: model });
-    this.$('#tweet-list').prepend(model.view.render()).hide().fadeIn('slow');
+    //this.$('#tweet-list').prepend(model.view.render().hide().fadeIn('slow'));
+    this.$('#tweet-entry').append(model.view.render());
   },
   reset: function() {
+    console.log("resting ...");
     this.$('#tweet-list').empty();
   },
   updateSearch: function(query) {
     this.collection.query = query;
     clearInterval(this.interval);
     this.collection.reset();
-    this.collection.fetch();
-    this.interval = setInterval(_.bind(this.collection.update, this.collection), 30000);
+    this.collection.fetch({success: this.emptyEntry});
+    //this.interval = setInterval(_.bind(this.collection.update, this.collection), 1000000);
+    this.interval = setInterval(_.bind(function() {
+      console.log("updating");
+      this.collection.fetch({remove: false, success: this.emptyEntryToStage});
+    }, this), 15000);
   },
   events: {
-    'click #searchButton': 'doSearch'
+    'click #searchButton': 'doSearch',
+    'click #tweet-stage-status': 'showMore'
   },
   doSearch: function() {
     var query = $('#searchBox').val();
     tweetRouter.navigate("#/search/"+query, {trigger: true});
+  },
+  emptyEntry: function() {
+    $('#tweet-entry li').appendTo('#tweet-list');
+  },
+  emptyEntryToStage: function() {
+    console.log("finished updating");
+    $('#tweet-entry li').prependTo('#tweet-stage');
+    var stage_count = $('#tweet-stage li').size();
+    if (stage_count > 0) {
+      $('#tweet-stage-status').show();
+      $('#tweet-stage-status').html(stage_count + " More tweets");
+    }
+  },
+  showMore: function() {
+    console.log("showmore");
+    $('#tweet-stage li').prependTo('#tweet-list');
+    $('#tweet-stage-status').hide();
   }
 });
 
